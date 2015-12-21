318b478dcb2481ccde6d389b1a8c04b0c07fd8a5
BHfeimao <xiaozhuang0706@gmail.com>
2015-12-21 14:55:55 +0800

    main

---
+++ PM25/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PM25.Startup))]
+namespace PM25
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
