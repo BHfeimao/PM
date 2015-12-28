979ae02df1e09108b0cb6da8a6ae85190a665445
jinhonglin <2211070303@qq.com>
2015-12-28 18:13:10 +0800

    shuaibiana

--- PM25/Controllers/SummaryController.cs
+++ PM25/Controllers/SummaryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PM25.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,9 @@ namespace PM25.Controllers
         // GET: Summary
         public ActionResult Detail()
         {
+            
             return View("Detail");
+
         }
     }
 }
