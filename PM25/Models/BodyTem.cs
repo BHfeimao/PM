d2b70af8c69399a54fafde3ad268bbcc3f46c421
BHfeimao <xiaozhuang0706@gmail.com>
2015-12-28 18:10:18 +0800

    create-bodytem-data

---
+++ PM25/Models/BodyTem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PM25.Models
+{
+    public class BodyTem
+    {
+        public int ID { get; set; }
+        public string Img { get; set; }
+        public string Summary { get; set; }
+        public string Detail { get; set; }
+    }
+    public class BodyTemDBContext : DbContext
+    {
+        public DbSet<BodyTem> BodyTems { get; set; }
+    }
+}
